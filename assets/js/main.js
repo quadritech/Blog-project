7a5a57979faeef5c1e3173eee3976931bebb30a7
quadritech <96469908+quadritech@users.noreply.github.com>
2022-05-27 04:47:15 +0100

    Header files done.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,22 @@
 // Grab elements
+const selectElement = selector => {
+    const elements = document.querySelector(selector)
+    if(elements) return element;
+    throw new Error('something went, make sure that $(selector)exists or is typed correctly.');
+};
 
+console.log(selectElement('.navbar'));
 //Nav styles on scroll
+const scrollHeader = () => {
+    const headerElement = selectElement('#header');
+    if(this.scrollY >= 15){
+        headerElement.classlist.add('activated');
+    }else{
+        headerElement.classlist.remove('activated');
+    }
+};
 
+window.addEventListener('scroll', scrollHeader);
 // Open menu & search pop-up
 
 // Open/Close search form popup
