d7734cda96a179510ab262739094174b1e450207
quadritech <96469908+quadritech@users.noreply.github.com>
2022-10-10 16:43:34 +0100

    back on the blog project after taking a break

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,11 +18,26 @@ const scrollHeader = () => {
 
 window.addEventListener('scroll', scrollHeader);
 // Open menu & search pop-up
+const menuToggleIcon = selectElement('menu-toggle-icon');
 
+const toggleMenu = () => {
+    const mobileMenu =  selectElement('menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+
+}
+
+menuToggleIcon.addEventListener('click', toggleMenu);
 // Open/Close search form popup
 
 // -- Close the search form popup on ESC keypress
 
 // Switch theme/add to local storage
+const bodyElement = document.body;
+const themeToggleBtn = selectElement('#theme-toggle-btn');
 
+themeToggleBtn.addEventListener('click', () => {
+    bodyElement.classList.toggle('light-theme');
+
+});
 // Swiper
